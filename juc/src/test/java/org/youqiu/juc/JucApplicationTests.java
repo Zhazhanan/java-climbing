b9f50953fb9a43bdf3811be4f9bb9bb6f5de0e89
wangkun <kunwang1@jieyuechina.com>
2020-04-28 16:58:48 +0800

    volatile 测试

---
+++ juc/src/test/java/org/youqiu/juc/JucApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youqiu.juc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JucApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
